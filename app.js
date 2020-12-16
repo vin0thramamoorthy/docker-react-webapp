ebe123d27732d80d39803cbf895c746127548547
Vinoth ramamoorthy <v-vr@youngliving.com>
2020-12-16 19:54:59 +0530

    update 80 port

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 80
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Good Morning')
 })
 
 app.listen(port, () => {
